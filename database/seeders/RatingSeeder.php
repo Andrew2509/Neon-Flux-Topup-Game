8eae648289add57aa25fe84d921922d8591e5233
Andrew <noreply@github.com>
2026-04-12 15:22:13 +0700

    Update testimonial names to real person names

--- database/seeders/RatingSeeder.php
+++ database/seeders/RatingSeeder.php
@@ -12,30 +12,33 @@ class RatingSeeder extends Seeder
      */
     public function run(): void
     {
+        // Bersihkan data lama agar nama-nama sebelumnya tidak menumpuk
+        Rating::truncate();
+
         $testimonials = [
             [
                 'product_name' => 'Mobile Legends',
                 'stars' => 5,
                 'comment' => 'Prosesnya cepet banget, gak sampe 5 menit diamond udah masuk. Mantap PrincePay!',
-                'author_nickname' => 'Rexy Gaming',
+                'author_nickname' => 'Ahmad Fauzi',
             ],
             [
                 'product_name' => 'Free Fire',
                 'stars' => 5,
                 'comment' => 'Awalnya ragu, tapi ternyata beneran aman dan murah. Langganan di sini terus deh.',
-                'author_nickname' => 'Sultan FF',
+                'author_nickname' => 'Siti Aminah',
             ],
             [
                 'product_name' => 'Valorant',
                 'stars' => 5,
                 'comment' => 'Gila sih harganya paling murah se-Indonesia. Adminnya juga ramah.',
-                'author_nickname' => 'ValoLovers',
+                'author_nickname' => 'Rizky Pratama',
             ],
             [
                 'product_name' => 'Genshin Impact',
                 'stars' => 5,
                 'comment' => 'Topup Genshin di sini paling worth it. Bonusnya banyak!',
-                'author_nickname' => 'Traveler ID',
+                'author_nickname' => 'Dwi Cahyo',
             ],
             [
                 'product_name' => 'Mobile Legends',
@@ -47,31 +50,31 @@ class RatingSeeder extends Seeder
                 'product_name' => 'Steam Wallet',
                 'stars' => 5,
                 'comment' => 'Situsnya user friendly, pembayarannya juga lengkap. Top pokoknya.',
-                'author_nickname' => 'Gamer Sejati',
+                'author_nickname' => 'Putri Indah',
             ],
             [
                 'product_name' => 'Mobile Legends',
                 'stars' => 5,
                 'comment' => 'Pelayanan bintang 5, sukses terus buat PrincePay Gaming!',
-                'author_nickname' => 'MLBB Mania',
+                'author_nickname' => 'Aditya Wijaya',
             ],
             [
                 'product_name' => 'PUBG Mobile',
                 'stars' => 5,
                 'comment' => 'Asli murah banget UC nya, auto borong skin!',
-                'author_nickname' => 'WinnerWinner',
+                'author_nickname' => 'Maya Sari',
             ],
             [
                 'product_name' => 'Call of Duty Mobile',
                 'stars' => 5,
                 'comment' => 'CP langsung masuk hitungan detik. Gak nyesel top up di sini.',
-                'author_nickname' => 'GhostRunner',
+                'author_nickname' => 'Eko Prasetyo',
             ],
             [
                 'product_name' => 'Roblox',
                 'stars' => 4,
                 'comment' => 'Robux nya murah-murah, beli banyak tetep hemat.',
-                'author_nickname' => 'Robloxian',
+                'author_nickname' => 'Ani Lestari',
             ],
         ];
 
